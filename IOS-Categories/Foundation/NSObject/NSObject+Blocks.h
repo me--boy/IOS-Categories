81d384309d4f092a3c72c5daad62969e6f5c50a4
maomecat <maomecat@126.com>
2015-05-04 21:44:53 +0800

    添加注释
    添加注释

--- IOS-Categories/Foundation/NSObject/NSObject+Blocks.h
+++ IOS-Categories/Foundation/NSObject/NSObject+Blocks.h
@@ -11,11 +11,15 @@
 @interface NSObject (Blocks)
 
 + (id)performBlock:(void (^)(void))block afterDelay:(NSTimeInterval)delay;
+
 + (id)performBlock:(void (^)(id arg))block withObject:(id)anObject afterDelay:(NSTimeInterval)delay;
+
 - (id)performBlock:(void (^)(void))block afterDelay:(NSTimeInterval)delay;
+
 - (id)performBlock:(void (^)(id arg))block withObject:(id)anObject afterDelay:(NSTimeInterval)delay;
 
 + (void)cancelBlock:(id)block;
+
 + (void)cancelPreviousPerformBlock:(id)aWrappingBlockHandle __attribute__ ((deprecated));
 
 @end
--- IOS-Categories/Foundation/NSObject/NSObject+Property.h
+++ IOS-Categories/Foundation/NSObject/NSObject+Property.h
@@ -9,7 +9,12 @@
 #import <Foundation/Foundation.h>
 
 @interface NSObject (Property)
+/**
+ *  返回对象中所有的属性以及属性的值
+ */
 -(NSDictionary *)propertyDictionary;
-
+/**
+ *  返回类中所有的属性列表
+ */
 + (NSArray *)classPropertyList;
 @end
